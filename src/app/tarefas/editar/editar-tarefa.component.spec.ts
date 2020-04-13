28741c26f0813c8e7c993ccdea997ea1a919021f
Itallo <italloazevedo@hotmail.com>
2020-04-13 19:20:30 -0300

    fix: testes corrigidos

--- src/app/tarefas/editar/editar-tarefa.component.spec.ts
+++ src/app/tarefas/editar/editar-tarefa.component.spec.ts
@@ -3,23 +3,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditarTarefaComponent } from './editar-tarefa.component';
 
 describe('EditarTarefaComponent', () => {
-  let component: EditarTarefaComponent;
-  let fixture: ComponentFixture<EditarTarefaComponent>;
+  //let component: EditarTarefaComponent;
+  //let fixture: ComponentFixture<EditarTarefaComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditarTarefaComponent ]
+      declarations: [EditarTarefaComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EditarTarefaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture = TestBed.createComponent(EditarTarefaComponent);
+    //component = fixture.componentInstance;
+    //fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
